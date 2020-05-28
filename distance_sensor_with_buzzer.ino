bca4aaf4e97134aad138abca14039f8f0ea6e4f3
smriti halder <65438599+smritihalder@users.noreply.github.com>
2020-05-28 15:52:02 +0530

    Add files via upload

---
+++ distance_sensor_with_buzzer.ino
@@ -0,0 +1,45 @@
+const int echo=11;
+const int trig=10;
+const int ledPin=13;
+const int buzzerPin=12; 
+
+int duration=0;
+int distance=0;
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+pinMode(ledPin,OUTPUT);
+pinMode(buzzerPin,OUTPUT);
+pinMode(trig,OUTPUT);
+pinMode(echo,INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(trig,OUTPUT);
+delayMicroseconds(1000);
+digitalWrite(trig,LOW);
+
+duration=pulseIn(echo,HIGH);
+distance=(duration/2)/28.5;
+Serial.println(distance);
+
+if(distance<=14){
+
+tone(buzzerPin,100);
+digitalWrite(ledPin,HIGH);
+delay(500);
+noTone(buzzerPin);
+digitalWrite(ledPin,LOW);
+delay(100);
+Serial.println("--------ALARM ACTIVATED------");
+}
+
+else{
+  noTone(buzzerPin);
+  digitalWrite(ledPin,LOW);
+
+  Serial.println("ALARM DEACTIVATED");
+}
+
+}
